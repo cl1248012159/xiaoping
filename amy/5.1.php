2c9e94e3966c055be296908de0fbe05be952f82c
leichen <313399524@qq.com>
2018-07-17 17:19:00 +0800

    5.1.php

--- amy/5.1.php
+++ amy/5.1.php
@@ -32,11 +32,35 @@
             </div>
             <div class="float_clear"></div>
         </div>
-        <div class="float_clear four_banner">
+        <div class="pagewidth float_clear four_banner">
             <?php for($i=0;$i<4;$i++){?>
                 <a href=""><img src="./images/test7.png"></a>
             <?php }?>
         </div>
+
+        <div class="pagewidth gmlist">
+            <?php for($i=0;$i<3;$i++){?>
+                <div class="gmblock">
+                    <div class="gmbtitle">
+                        家用电器
+                        <a href="">更多</a>
+                    </div>
+                    <ul class="gmblist">
+                        <?php for($j=0;$j<10;$j++){ ?>
+                            <li class="gmbitem">
+                                <a href="">
+                                    <img src="./images/img_01.png">
+                                    <span class="gmbiname">Skyworth/ 55H5 55寸智能网络液晶平板wifi电视机官方旗舰店</span>
+                                    <span class="gmbiprice">¥2600.00</span>
+                                </a>
+                            </li>
+                        <?php }?>
+                    </ul>
+                    <div class="float_clear"></div>
+                </div>
+            <?php } ?>
+        </div>
+        <?php include('./public/pages.php');?>
     </div>
     
 </body>
