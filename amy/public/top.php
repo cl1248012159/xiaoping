9ebb710de3a7879c8e8bca21a996bde317d67889
leichen <313399524@qq.com>
2018-07-18 16:17:32 +0800

    bug

--- amy/public/top.php
+++ amy/public/top.php
@@ -14,5 +14,21 @@
         <li><a href="">登录</a></li>
         <li><a href="">注册</a></li>
     </ul>
+
+    <!-- 已登录 -->
+    <div class="alogin">
+        <a class="menu-hd myinfo" href="http://www.huabaowang.cn/index.php?s=/member/index" target="_blank" style="">myname<b style="background-image: url('./images/shopimg_01.png')"></b></a>
+        <div id="menu-2" class="menu-bd">
+            <span class="menu-bd-mask"></span>
+            <div class="menu-bd-panel">
+                <a href="http://www.huabaowang.cn/index.php?s=/member/index" target="_blank">个人中心</a>
+                <a href="http://www.huabaowang.cn/index.php?s=/member/orderlist" target="_blank">已买宝贝</a>
+                <a href="http://www.huabaowang.cn/index.php?s=/member/addresslist" target="_blank">地址管理</a>
+                <a href="http://www.huabaowang.cn/index.php?s=/member/goodscollectionlist" target="_blank">我的收藏</a>
+                <a href="javascript:logout();">退出登录</a>
+            </div>
+        </div>
+    </div>
+
     <div class="float_clear"></div>
 </div>
